ceedddd8633f292946ff45920f0e637e388aa3d1
cyau76 <127944745+cyau76@users.noreply.github.com>
2026-02-25 00:59:52 -0800

    Remove React integration from Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import sitemap from "@astrojs/react";
+
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://cyau76.com",
-	integrations: [mdx(), sitemap(),react()],
+	integrations: [mdx(), sitemap()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
