12f34ff5e5a60a95d7cbdc0b4d5adcf2320686bc
cyau76 <127944745+cyau76@users.noreply.github.com>
2026-02-25 00:58:31 -0800

    Add React integration to Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,14 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import sitemap from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://cyau76.com",
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(),react()],
 	adapter: cloudflare({
 		platformProxy: {
 			enabled: true,
